d7ab1bf3fa8b3a33b88a55d809ff7049da95ba75
Yonatan Tussa <140031110+yonatantussa@users.noreply.github.com>
2025-01-02 00:55:14 -0500

    feat: add custom navbar

--- SONEApp/app/(tabs)/_layout.tsx
+++ SONEApp/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
+import { CustomTabBar } from '@/components/CustomTabBar';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -13,9 +14,11 @@ export default function TabLayout() {
 
   return (
     <Tabs
+      tabBar={props=> <CustomTabBar {...props} />}
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        /*
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
@@ -25,19 +28,28 @@ export default function TabLayout() {
           },
           default: {},
         }),
-      }}>
+        */
+      }}
+    >
+      <Tabs.Screen
+        name="patients"
+        options={{
+          title: 'Patients',
+          tabBarIcon: ({ color }) => <IconSymbol size={35} name="person.3.fill" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={35} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="settings"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <IconSymbol size={35} name="gearshape.fill" color={color} />,
         }}
       />
     </Tabs>
